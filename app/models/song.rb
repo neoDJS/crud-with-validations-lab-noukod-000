6252728ea7e6467e70d0f21104110613fefe9c4d
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-03-21 17:42:21 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,7 @@
-class Song < ApplicationRecord
-  validates :title, presence: true
+class Song < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: { scope: [:artist_name, :release_year],
+    message: "should happen once per year with the same artist" }
+  validates :released, inclusion: { in: [true false] }
+  validates :release_year, presence: true, if: Proc.new{|s| (s.released == true) and (s.release_year <= Time.new.year) }
   validates :artist_name, presence: true
 end
