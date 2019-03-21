a6b438677fccaa1c07245adf1a69d39b53685380
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-03-21 17:52:30 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,23 @@
 class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+  end
 end
